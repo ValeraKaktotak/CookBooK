dcf55106934bf14f8af75862e840e4337ce6b6f6
Valera <valerakaktotak@gmail.com>
2021-12-11 23:24:32 +0200

    add new theme cookbook

---
+++ wp-content/themes/cookbook/header.php
@@ -0,0 +1,39 @@
+<html>
+<title>
+    TEST PAGE
+</title>
+<head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" >
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-utilities.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" class="nav-link px-2 text-white text-decoration-none logo">Logo</a></li>
+                <li><a href="#" class="nav-link px-2 text-secondary">Home</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">Countries</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">Pricing</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login
+                </button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
+        </div>
+    </div>
+</header>
