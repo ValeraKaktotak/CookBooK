232f5321fd66d14e4d6b9026a41810ed820529a0
Valera <valerakaktotak@gmail.com>
2021-12-15 10:03:59 +0200

    fixed tag img

--- wp-content/themes/cookbook/taxonomy-categories.php
+++ wp-content/themes/cookbook/taxonomy-categories.php
@@ -4,17 +4,15 @@
     <div class="row">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
         <div class="col">
+            <h1>
+                <?php the_title();?>
+            </h1>
+            <img class="card-img-top" src="<?php echo get_the_post_thumbnail_url(); ?>" />
 
-                <h1>
-                    <?php the_title();?>
-                </h1>
-                <img class="card-img-top" src="<?php echo get_the_post_thumbnail_url(); ?>"
-
-                <div class="btn-group margin-20">
-                    <a class="btn btn-secondary btn-lg" href="<?php the_permalink(); ?>"
-                       role="button">View details »</a>
-                </div>
-
+            <div class="btn-group margin-20">
+                <a class="btn btn-secondary btn-lg" href="<?php the_permalink(); ?>"
+                   role="button">View details »</a>
+            </div>
         </div>
         <?php endwhile;
         else :
