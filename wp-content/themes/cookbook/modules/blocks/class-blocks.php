3d0411a77174ad4c86c21401df9fb8b9bd068b45
Valera <valerakaktotak@gmail.com>
2021-12-21 23:12:59 +0200

    change file name class-blocks, add blocks to one config cloass blocks, add dishes content in template-part

--- wp-content/themes/cookbook/modules/blocks/class-blocks.php
+++ wp-content/themes/cookbook/modules/blocks/class-blocks.php
@@ -2,27 +2,21 @@
 namespace CookBook\Modules\Blocks;
 class Blocks {
     public function __construct() {
-        add_action( 'acf/init', [ $this, 'banner_block' ] );
-        add_action( 'acf/init', [ $this, 'banner_slider_block' ] );
-        add_action( 'acf/init', [ $this, 'recipe_block' ] );
+        add_action( 'acf/init', [ $this, 'blocks_registers' ] );
     }
-    public function banner_block() {
+    public function blocks_registers() {
         acf_register_block_type( [
             'name'            => 'bg_banner',
             'title'           => __( 'Dishes Banner Block', DISHES_DB_TEXT_DOMAIN ),
             'render_template' => 'template-parts/blocks/banner-block.php',
             'mode'            => 'edit',
         ] );
-    }
-    public function banner_slider_block() {
         acf_register_block_type( [
             'name'            => 'chef-slider',
             'title'           => __( 'Banner Slider Block', DISHES_DB_TEXT_DOMAIN ),
             'render_template' => 'template-parts/blocks/home-bg-slider.php',
             'mode'            => 'edit',
         ] );
-    }
-    public function recipe_block() {
         acf_register_block_type( [
             'name'            => 'dish-recipe',
             'title'           => __( 'Recipe Block', DISHES_DB_TEXT_DOMAIN ),
