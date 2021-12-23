1fb95d7df5e0fe25fabae58293958a820e6f07fd
Valera <valerakaktotak@gmail.com>
2021-12-23 13:55:43 +0200

    fix mistakes in config file and class file

--- wp-content/themes/cookbook/modules/blocks/class-blocks.php
+++ wp-content/themes/cookbook/modules/blocks/class-blocks.php
@@ -4,6 +4,7 @@ namespace CookBook\Modules\Blocks;
 class Blocks {
     public function __construct() {
         add_action( 'acf/init', [ $this, 'register_blocks' ] );
+        add_action( 'acf/init', [ $this, 'register_option' ]);
     }
 
     private function get_config(): array {
@@ -18,4 +19,66 @@ class Blocks {
         }
     }
 
+
+    function register_option() {
+        acf_add_options_page( [
+            'page_title' => 'My site phone number',
+            'menu_title' => 'Phone number Settings',
+            'menu_slug'  => 'phone-number-settings',
+            'capability' => 'edit_posts',
+            'redirect'   => false,
+        ] );
+        acf_add_options_page( [
+            'page_title' => 'My site logo',
+            'menu_title' => 'Logo Settings',
+            'menu_slug'  => 'logo-settings',
+            'capability' => 'edit_posts',
+            'redirect'   => false,
+        ] );
+    }
+
 }
+
+/*
+class Blocks {
+    public function __construct() {
+        add_action( 'acf/init', [ $this, 'blocks_registers' ] );
+        add_action( 'acf/init', [ $this, 'register_option' ]);
+    }
+    public function blocks_registers() {
+        acf_register_block_type( [
+            'name'            => 'bg_banner',
+            'title'           => __( 'Dishes Banner Block', DISHES_DB_TEXT_DOMAIN ),
+            'render_template' => 'template-parts/blocks/banner-block.php',
+            'mode'            => 'edit',
+        ] );
+        acf_register_block_type( [
+            'name'            => 'chef-slider',
+            'title'           => __( 'Banner Slider Block', DISHES_DB_TEXT_DOMAIN ),
+            'render_template' => 'template-parts/blocks/home-bg-slider.php',
+            'mode'            => 'edit',
+        ] );
+        acf_register_block_type( [
+            'name'            => 'dish-recipe',
+            'title'           => __( 'Recipe Block', DISHES_DB_TEXT_DOMAIN ),
+            'render_template' => 'template-parts/blocks/recipe.php',
+            'mode'            => 'edit',
+        ] );
+    }
+    function register_option() {
+        acf_add_options_page( [
+            'page_title' => 'My site phone number',
+            'menu_title' => 'Phone number Settings',
+            'menu_slug'  => 'phone-number-settings',
+            'capability' => 'edit_posts',
+            'redirect'   => false,
+        ] );
+        acf_add_options_page( [
+            'page_title' => 'My site logo',
+            'menu_title' => 'Logo Settings',
+            'menu_slug'  => 'logo-settings',
+            'capability' => 'edit_posts',
+            'redirect'   => false,
+        ] );
+    }
+}*/
--- wp-content/themes/cookbook/modules/blocks/config/acf-blocks.php
+++ wp-content/themes/cookbook/modules/blocks/config/acf-blocks.php
@@ -19,18 +19,5 @@ return [
         'render_template' => 'template-parts/blocks/recipe.php',
         'mode'            => 'edit',
     ],
-    [
-        'page_title' => 'My site phone number',
-        'menu_title' => 'Phone number Settings',
-        'menu_slug'  => 'phone-number-settings',
-        'capability' => 'edit_posts',
-        'redirect'   => false,
-    ],
-    [
-        'page_title' => 'My site logo',
-        'menu_title' => 'Logo Settings',
-        'menu_slug'  => 'logo-settings',
-        'capability' => 'edit_posts',
-        'redirect'   => false,
-    ]
+
 ];
